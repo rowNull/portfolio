3071a15c5da1e67e4a0490d63b6a5c67de491228
rowNull <129787371+rowNull@users.noreply.github.com>
2025-05-29 01:57:24 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
+// Scroll Up to top function
 const scrollUp = document.querySelector("#scroll-up");
 scrollUp.addEventListener("click", ()=>{
     window.scrollTo({top: 0,left:0, behavior: "smooth"});
-    });
+});
 
+
+// menu toggle for mobile and tablets
 const menu = document.querySelector("#menu-toggle");
 const ul = document.querySelector("nav ul");
 
@@ -10,15 +13,15 @@ menu.addEventListener("click", () => {
     ul.classList.toggle("show");
 });
 
-
 const navLink = document.querySelectorAll(".nav-link");
-
 navLink.forEach((link) =>
-link.addEventListener("click", () => {
-    ul.classList.remove("show");
-})
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
 );
 
+
+// form submission
 const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
     e.preventDefault(); 
